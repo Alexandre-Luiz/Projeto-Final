c231926c6455a9eca429e9ae2f1c7662750f0f64
Alexandre Luiz Elias <alexandre.alpe@gmail.com>
2020-09-09 06:03:20 -0300

    Questions na seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,28 +4,30 @@ user = User.create!(name: 'Gabriel', password: '123456789',
                     email: 'gabriel@google.com', role: 'Estagiário',
                     department: 'Recursos humanos')
 user_2 = User.create!(name: 'Rafael', password: '123456789', 
-                    email: 'rafael@google.com', role: 'Diretor',
-                    department: 'Financeiro')
+                      email: 'rafael@google.com', role: 'Diretor',
+                      department: 'Financeiro')
 user_3 = User.create!(name: 'Felipe', password: '123456789', 
-                    email: 'felipe@google.com', role: 'Gerente',
-                    department: 'marketing')
+                      email: 'felipe@google.com', role: 'Gerente',
+                      department: 'marketing')
 
 product = Product.create!(name: 'Placa de vídeo GTX 970', category: 'Eletrônicos', 
-                        description: 'Funcionando perfeitamente', price: '1000', 
-                        user: user, status: :enabled)
+                          description: 'Funcionando perfeitamente', price: '1000', 
+                          user: user, status: :enabled)
 product_2 = Product.create!(name: 'Bola oficial copa 2014', category: 'Esporte', 
-                        description: 'Nunca foi usada', price: '2000', 
-                        user: user, status: :enabled)
+                            description: 'Nunca foi usada', price: '2000', 
+                            user: user, status: :enabled)
 product_3 = Product.create!(name: 'Monitor Samsung 24 polegadas', category: 'Eletrônicos', 
-                        description: 'Um ano de uso. Não apresenta nenhum defeito', price: '1000', 
-                        user: user_2, status: :enabled)
+                            description: 'Um ano de uso. Não apresenta nenhum defeito', price: '1000', 
+                            user: user_2, status: :enabled)
 product_4 = Product.create!(name: 'Jaqueta de couro legítimo', category: 'Roupas e vestuário', 
-                        description: 'Comprei, mas não serviu. Nunca foi usada', price: '400', 
-                        user: user_3, status: :enabled)
+                            description: 'Comprei, mas não serviu. Nunca foi usada', price: '400', 
+                            user: user_3, status: :enabled)
+
+Question.create!(content: 'É autografada pelo Neymar?', user: user_3, product: product_2)
+Question.create!(content: 'Qual o tamanho? Serve em alguem de 2,20?', user: user, product: product_4)
 
 
-
-# Usuários e empresa 2
+# Usuários e produtos empresa 2
 user_4 = User.create!(name: 'João', password: '123456789', 
                       email: 'João@facebook.com', role: 'Estagiário',
                       department: 'Segurança da informação')
@@ -48,3 +50,6 @@ product_6 = Product.create!(name: 'Playstation 4', category: 'Brinquedos e games
 product_7 = Product.create!(name: 'Atari', category: 'Brinquedos e games ', 
                             description: 'Muito bem conservado', 
                             price: 5000, user: user_6, status: :enabled)
+
+Question.create!(content: 'Vem com mais de um controle?', user: user_6, product: product_6)
+Question.create!(content: 'É banhado a ouro?', user: user_5, product: product_5)
